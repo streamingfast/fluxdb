3b034f0f9757275acb3ee300c99f9bf4f4708cc4
Matthieu Vachon <matt@dfuse.io>
2020-08-25 15:44:57 -0400

    Small tweaks to tests

--- sharding_test.go
+++ sharding_test.go
@@ -50,17 +50,17 @@ func runTests(t *testing.T, scratchDirectory string) {
 	singlet1 := newTestSinglet("sg1")
 	singlet2 := newTestSinglet("sg2")
 
-	streamBlock(t, sharder, "00000001aa", writeRequest(
+	streamBlock(t, sharder, "00000001aa", "", writeRequest(
 		[]SingletEntry{singlet1.entry(t, 1, "s1 e #1")},
 		[]TabletRow{tablet1.row(t, 1, "001", "t1 r1 #1"), tablet1.row(t, 1, "002", "t1 r2 #1")}),
 	)
 
-	streamBlock(t, sharder, "00000002aa", writeRequest(
+	streamBlock(t, sharder, "00000002aa", "", writeRequest(
 		[]SingletEntry{singlet2.entry(t, 2, "s2 e #2")},
 		[]TabletRow{tablet2.row(t, 2, "001", "t2 r1 #2"), tablet2.row(t, 2, "002", "t2 r2 #2")}),
 	)
 
-	streamBlock(t, sharder, "00000003aa", writeRequest(
+	streamBlock(t, sharder, "00000003aa", "", writeRequest(
 		[]SingletEntry{singlet1.entry(t, 3, "s1 e #3"), singlet2.entry(t, 3, "s2 e #3")},
 		[]TabletRow{
 			tablet1.row(t, 3, "002", "t1 r2 #3"),
@@ -122,8 +122,8 @@ func runTests(t *testing.T, scratchDirectory string) {
 	assert.Equal(t, []TabletRow{tablet2.row(t, 3, "001", "t2 r1 #3"), tablet2.row(t, 2, "002", "t2 r2 #2")}, tablet2Rows)
 }
 
-func streamBlock(t *testing.T, sharder *Sharder, id string, request *WriteRequest) {
-	blk := bblock(id)
+func streamBlock(t *testing.T, sharder *Sharder, id, libID string, request *WriteRequest) {
+	blk := bblock(id, libID)
 	request.Height = blk.Num()
 	request.BlockRef = blk.AsRef()
 
@@ -132,7 +132,7 @@ func streamBlock(t *testing.T, sharder *Sharder, id string, request *WriteReques
 }
 
 func endBlock(t *testing.T, sharder *Sharder, id string) {
-	blk := bblock(id)
+	blk := bblock(id, "")
 	req := &WriteRequest{
 		Height:   blk.Num(),
 		BlockRef: blk.AsRef(),
@@ -149,14 +149,19 @@ func writeRequest(entries []SingletEntry, rows []TabletRow) *WriteRequest {
 	}
 }
 
-func bblock(id string) *bstream.Block {
+func bblock(id, libID string) *bstream.Block {
 	ref := bstream.NewBlockRefFromID(id)
 	fork := id[8:]
+
+	libNum := ref.Num() - 1
+	if libID != "" {
+		libNum = bstream.NewBlockRefFromID(id).Num()
+	}
 
 	return &bstream.Block{
 		Id:         ref.ID(),
 		Number:     ref.Num(),
-		LibNum:     ref.Num() - 1,
+		LibNum:     libNum,
 		PreviousId: fmt.Sprintf("%08x%s", uint32(ref.Num()-1), fork),
 		Timestamp:  time.Now(),
 	}
